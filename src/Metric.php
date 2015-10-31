626b55b0b133d8062175e46f726d092f3ca4d31a
Akh0 <achille.guillon@xotelia.com>
2015-10-31 20:03:40 +0100

    Initial commit

---
+++ src/Metric.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Achille
+ * Date: 31/10/2015
+ * Time: 19:01
+ */
---
+++ src/SaasMetrics.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Achille
+ * Date: 31/10/2015
+ * Time: 15:38
+ */
